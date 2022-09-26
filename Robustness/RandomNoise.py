0a1f4f9df443f2c3c959fe9da87a4786c6bd5a59
ifo002 <isafoster@live.com>
2022-09-26 22:17:29 +0200

    cleanup

--- Robustness/RandomNoise.py
+++ Robustness/RandomNoise.py
@@ -1,16 +1,10 @@
 import numpy as np
 from sklearn import metrics
 
-def plus(numbers: list):
-    value = 0 
-    for number in numbers:
-        value += number
-    return value
-
-
 def addNoise(X, scale=0.1):
     print(X)
     return X + np.random.normal(0, scale, X.shape)
+    ## should only affect a certain percentage of the datapoints?? 
 
 def evaluate(model, X_train, y_train, X_test, y_test, values= np.linspace(0, 0.5, 30)):
     accuracy_scores = []
