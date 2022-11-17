66287c89c262a932096606e5213b495cf34aa1b1
Isabel <isafoster@live.com>
2022-11-17 20:10:05 +0100

    removed duplicate methods and import cleanup

--- Robustness/figure5.py
+++ Robustness/figure5.py
@@ -1,15 +1,12 @@
-from readData import getData
-from sampling import sampleData
+from _readData import getData
+from _sampling import sampleData
+from _plot import plotAll
 import numpy as np
-from matplotlib import pyplot as plt
 import numpy as np
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
 import pandas as pd
-import plotly.graph_objs as go
-from plotly.offline import plot
-import plotly.express as px
 from tqdm import tqdm
 
 '*********** Load and Split Data ***********'
@@ -46,75 +43,12 @@ def addNoiseColumn(feature, factor):
     a = feature + noise
     return a
 
-'***********************************************'
-
 def sort_df(df):
     df_temp = df.copy()
-    df_temp.sort_values(by=['average_value'])
+    df_temp.sort_values(by=['feature_name'])
     return df_temp
 
-def top5features(df):
-    #df_ordered = df.loc[df['level'] == 0.0].groupby('feature_name').nlargest(5)
-    #print(df_ordered.head(5))
-
-    df_grouped = df.groupby('level')
-    df_group = df_grouped.get_group(0.0)
-    sorted = df_group.sort_values('average_value', ascending=False)
-    return sorted['feature_name'].tolist()[:5]
-
-def mostDiff(df):
-    df_temp = pd.DataFrame(columns=['feature_name', 'most_diff'])
-    df_grouped = df.groupby('feature_name')
-    for group in df_grouped:
-        li = group[1]['average_value'].tolist()
-        diff = max(li)- min(li)
-        row = pd.Series([group[0], diff])
-        row = pd.DataFrame([[group[0], diff]], columns=['feature_name', 'most_diff'])
-        df_temp = pd.concat([df_temp, row], axis=0)
-    sorted = df_temp.sort_values('most_diff', ascending=False)
-    return sorted['feature_name'].tolist()[:5]
-
-def plotAll(df):
-    visible_features = top5features(df)
-    different_features = mostDiff(df)
-    title = "Average feature importance over 100 replacement noise corruptions at increasing noise levels"
-    fig = px.line(df, x="level", y="average_value", title=title, color='feature_name').update_traces(visible="legendonly", selector=lambda t: not t.name in visible_features) 
-    fig.update_layout(dict(updatemenus=[
-                        dict(
-                            type = "buttons",
-                            direction = "left",
-                            buttons=list([
-                                dict(
-                                    args=["visible", "legendonly"],
-                                    label="Deselect All",
-                                    method="restyle"
-                                ),
-                                dict(
-                                    args=["visible", True],
-                                    label="Select All",
-                                    method="restyle"
-                                ),
-                                dict(
-                                    args=[{"visible": [i in visible_features for i in df['feature_name'].unique()]}],
-                                    label="Top 5",
-                                    method="restyle"
-                                ),
-                                dict(
-                                    args=[{"visible": [i in different_features for i in df['feature_name'].unique()]}],
-                                    label="Most diff",
-                                    method="restyle"
-                                )
-                            ]),
-                            pad={"r": 10, "t": 10},
-                            showactive=False,
-                            x=1,
-                            xanchor="right",
-                            y=1.1,
-                            yanchor="top"
-                        ),
-                    ]
-              ))
-    fig.show()
+'***********************************************'
 
 def doAll(df, X_test, y_test, model, corruptions=10, levels=np.linspace(0, 1, 11)):
     pbar_outer = tqdm(levels, desc="Total progress: ", position=0)
@@ -136,8 +70,10 @@ def doAll(df, X_test, y_test, model, corruptions=10, levels=np.linspace(0, 1, 11
             corrupted_noise = addNoiseDf(X, level)
             model.fit(corrupted_noise, y.values.ravel())
             if hasattr(model, 'feature_importances_'):
+                measured = 'feature importance'
                 parameter_values.append(model.feature_importances_)
             elif hasattr(model, 'coef_'):
+                measured = 'coefficients'
                 parameter_values.append(model.coef_)
             else:
                 print("cound not calculate coefficients or feature importance")
@@ -151,7 +87,7 @@ def doAll(df, X_test, y_test, model, corruptions=10, levels=np.linspace(0, 1, 11
         df_temp = pd.DataFrame({'feature_name': feature_names, 'average_value': average_level_value.flatten(), 'level': np.array([level]*len(feature_names))})
         df_plot = pd.concat([df_plot, df_temp], axis=0)
     df_plot = sort_df(df_plot)
-    plotAll(df_plot)
+    plotAll(df_plot, str(model), measured, corruptions)
     return df_plot
 
 model_1 = RandomForestClassifier(random_state=42)
