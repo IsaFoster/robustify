50845cbadf3f679cfa54665c5518299a38a32806
Isabel <isafoster@live.com>
2022-11-17 20:08:31 +0100

    removed old files

--- Robustness/sampling.py
+++
@@ -1,5 +0,0 @@
-def sampleData(df, frac):
-    df_sampled = df.sample(frac=frac, replace=True)
-    y_sampled = df_sampled[['data_type']]
-    X_sampled = df_sampled.drop(['data_type'], axis=1)
-    return X_sampled, y_sampled
