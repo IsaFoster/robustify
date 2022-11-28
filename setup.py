12371c56f77c8c71de0b950d4e04513aa601f736
Isabel <isafoster@live.com>
2022-11-29 00:09:44 +0100

    updated setup.py with versions

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ setup(
     description='no description',
     author='Isabel Foster',
     license='MIT',
-    install_requires=['numpy=1.23.4', 
-                      'sklearn=1.1.3', 
-                      'matplotlib=3.6.2', 
-                      'pandas=1.5.1', 
-                      'plotly=5.11.0', 
-                      'tqdm=4.64.1'], # list of packages that are absolutely needed, not standard library
+    install_requires=['numpy==1.23.4', 
+                      'sklearn==1.1.3', 
+                      'matplotlib==3.6.2', 
+                      'pandas==1.5.1', 
+                      'plotly==5.11.0', 
+                      'tqdm==4.64.1'], # list of packages that are absolutely needed, not standard library
     setup_requires=['pytest-runner'], # only installed when required to run tests
     tests_require=['pytest==7.1.2'],
     test_suite='tests',
