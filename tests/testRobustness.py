500d510849be18fba2c45e8aff81f441769624e1
Isabel <isafoster@live.com>
2022-11-28 20:12:51 +0100

    written script for testing github actions

--- tests/testRobustness.py
+++
@@ -1,8 +0,0 @@
-from libraryTestCERN import testFunctions
-
-def test_plus():
-    assert testFunctions.plus([5,2,3]) == 10
-
-
-def test_plus_decimals():
-    assert testFunctions.plus([5.5,2.2,3.7]) == 11.4
