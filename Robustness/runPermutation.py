7f30ee90392dc8f1f1bf05f726dea150b479a771
ifo002 <isafoster@live.com>
2023-01-17 19:46:09 +0100

    self defined score functions

--- Robustness/runPermutation.py
+++ Robustness/runPermutation.py
@@ -15,23 +15,17 @@ random_state = 44
 
 _, _, X_test, y_test = getXandYShortFromFile()
 
-def accuracy_keras(estimator, X, y):
-    pred = estimator.predict(X) 
-    pred = np.argmax(pred, axis = 1)
-    return accuracy_score(y, pred)
-
-def accuacy_keras_2(y_true, y_pred):
+def self_defined_accuracy(y_true, y_pred):
     pred = np.argmax(y_pred, axis = 1)
     return accuracy_score(y_true.values, pred)
 
-def permuteModel(modelName, X_test, y_test, n_repeats, random_state, scoring):
+def permuteModel(modelName, X_test, y_test, n_repeats, random_state, scoring=None):
     model = pickle.load(open('../Models/' + modelName, 'rb'))
 
     permutationImportance(model, X_test, y_test, n_repeats, random_state, scoring)
     meanAccuracyDecrease(model, X_test, y_test, n_repeats, random_state, scoring)
 
-#permuteModel('RF_full_set', X_test, y_test, n_repeats, random_state)
-#permuteModel('SVC_full_set', X_test, y_test, n_repeats, random_state)
-#permuteModel('LDA_full_set', X_test, y_test, n_repeats, random_state)
-#permuteModel('SK_reduced_set', X_test, y_test, n_repeats, random_state, scoring='r2')
-permuteModel('SK_reduced_set', X_test, y_test, n_repeats, random_state, scoring=make_scorer(accuacy_keras_2))
+permuteModel('RF_full_set', X_test, y_test, n_repeats, random_state)
+permuteModel('SVC_full_set', X_test, y_test, n_repeats, random_state)
+permuteModel('LDA_full_set', X_test, y_test, n_repeats, random_state)
+permuteModel('SK_reduced_set', X_test, y_test, n_repeats, random_state, scoring=make_scorer(self_defined_accuracy, greater_is_better=True))
