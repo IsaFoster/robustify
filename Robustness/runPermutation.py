3559bd9bd2f185dd1f9216c86313798645d91bf3
ifo002 <isafoster@live.com>
2023-01-17 15:12:03 +0100

    permutation importance for keras model "r2" score

--- Robustness/runPermutation.py
+++ Robustness/runPermutation.py
@@ -3,6 +3,8 @@ from permutationImportance import permutationImportance, meanAccuracyDecrease
 import numpy as np
 import random
 import pickle
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import make_scorer
 
 seed = 39
 random.seed(seed)
@@ -13,13 +15,23 @@ random_state = 44
 
 _, _, X_test, y_test = getXandYShortFromFile()
 
+def accuracy_keras(estimator, X, y):
+    pred = estimator.predict(X) 
+    pred = np.argmax(pred, axis = 1)
+    return accuracy_score(y, pred)
 
-def permuteModel(modelName, X_test, y_test, n_repeats, random_state):
+def accuacy_keras_2(y_true, y_pred):
+    pred = np.argmax(y_pred, axis = 1)
+    return accuracy_score(y_true, pred)
+
+def permuteModel(modelName, X_test, y_test, n_repeats, random_state, scoring):
     model = pickle.load(open('../Models/' + modelName, 'rb'))
 
-    permutationImportance(model, X_test, y_test, n_repeats, random_state)
-    meanAccuracyDecrease(model, X_test, y_test, n_repeats, random_state)
+    permutationImportance(model, X_test, y_test, n_repeats, random_state, scoring)
+    meanAccuracyDecrease(model, X_test, y_test, n_repeats, random_state, scoring)
 
-permuteModel('RF_full_set', X_test, y_test, n_repeats, random_state)
-permuteModel('SVC_full_set', X_test, y_test, n_repeats, random_state)
-permuteModel('LDA_full_set', X_test, y_test, n_repeats, random_state)
+#permuteModel('RF_full_set', X_test, y_test, n_repeats, random_state)
+#permuteModel('SVC_full_set', X_test, y_test, n_repeats, random_state)
+#permuteModel('LDA_full_set', X_test, y_test, n_repeats, random_state)
+permuteModel('SK_reduced_set', X_test, y_test, n_repeats, random_state, scoring='r2')
+#permuteModel('SK_reduced_set', X_test, y_test, n_repeats, random_state, scoring=make_scorer(accuacy_keras_2, greater_is_better=True))
--- Setup/createAndSaveModel.py
+++ Setup/createAndSaveModel.py
@@ -4,6 +4,7 @@ from sklearn.svm import SVC
 import pickle
 from sklearn.neural_network import MLPClassifier
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from tensorflow import keras
 
 '************* Load Data *******************'
 X_train, y_train, _, _ = getXandYFromFile()
@@ -51,4 +52,33 @@ model = LinearDiscriminantAnalysis()
 model.fit(X_train_short, y_train_short.values.ravel())
 pickle.dump(model, open('../Models/' + modelName, 'wb'))
 
+modelName = "SK_reduced_set"
+model = keras.Sequential([
+    keras.layers.Dense(units=X_train_short.shape[1], activation="relu", input_shape=(X_train_short.shape[-1],) ),
+    keras.layers.Dropout(0.3),
+    keras.layers.Dense(units=X_train_short.shape[1], activation="relu"),
+    keras.layers.Dense(units=1, activation="sigmoid"),])
+model.compile(optimizer=keras.optimizers.Adam(learning_rate=0.001), 
+            loss="binary_crossentropy", 
+            metrics=keras.metrics.AUC())
+model.fit(X_train_short, y_train_short, 
+        epochs=500, 
+        batch_size=1000,
+        verbose=0)
+pickle.dump(model, open('../Models/' + modelName, 'wb'))
+
+modelName = "SK_full_set"
+model = keras.Sequential([
+    keras.layers.Dense(units=X_train.shape[1], activation="relu", input_shape=(X_train.shape[-1],) ),
+    keras.layers.Dropout(0.3),
+    keras.layers.Dense(units=X_train.shape[1], activation="relu"),
+    keras.layers.Dense(units=1, activation="sigmoid"),])
+model.compile(optimizer=keras.optimizers.Adam(learning_rate=0.001), 
+            loss="binary_crossentropy", 
+            metrics=keras.metrics.AUC())
+model.fit(X_train, y_train, 
+        epochs=500, 
+        batch_size=1000,
+        verbose=0)
+pickle.dump(model, open('../Models/' + modelName, 'wb'))
 '*******************************************'
