03d66899efa820e6f3aa5949898145e49f5f118c
Isabel <isafoster@live.com>
2023-02-14 19:29:36 +0100

    Issue #20 and #17 gaussian noise fix

--- Noise/continuous.py
+++ Noise/continuous.py
@@ -1,9 +1,8 @@
 import numpy as np
 
-def Gaussian_Noise(clean_cata, percentage, random_state=None):
+def Gaussian_Noise(clean_data, percentage, random_state=None):
     np.random.seed(random_state)
-    sd = np.std(clean_cata)
-    noise = np.random.normal(0, sd, len(clean_cata))
-    corrupted_data = clean_cata + (noise * (1 + percentage))
-    return corrupted_data
+    noiseSigma = clean_data * percentage
+    noise = noiseSigma * np.random.normal(0, 1, len(clean_data))
+    return clean_data + noise
 
--- tests/test_gaussianNoise.py
+++ tests/test_gaussianNoise.py
@@ -2,12 +2,47 @@ from Noise.continuous import Gaussian_Noise
 import numpy as np
 import pandas as pd
 
-test_data = np.array([100,60,130,122,99,87,103, 101,65,110,72,89,82,117,111,70,140,109,79,67,113,119,66,132,112,98,89,104,101,80,120,124,98, 100,60,130,122,99,87,103, 101,65,110,72,89,82,117,111,70,140,109,79,67,113,119,66,132,112,98,89,104,101,80,120,124,98, 100,60,130,122,99,87,103, 101,65,110,72,89,82,117,111,70,140,109,79,67,113,119,66,132,112,98,89,104,101,80,120,124,98, 100,60,130,122,99,87,103, 101,65,110,72,89,82,117,111,70,140,109,79,67,113,119,66,132,112,98,89,104,101,80,120,124,98, 100,60,130,122,99,87,103, 101,65,110,72,89,82,117,111,70,140,109,79,67,113,119,66,132,112,98,89,104,101,80,120,124,98, 100,60,130,122,99,87,103, 101,65,110,72,89,82,117,111,70,140,109,79,67,113,119,66,132,112,98,89,104,101,80,120,124,98, 100,60,130,122,99,87,103, 101,65,110,72,89,82,117,111,70,140,109,79,67,113,119,66,132,112,98,89,104,101,80,120,124,98])
-df = pd.DataFrame(data=test_data)
-percentage = 0.2
-noisy_data = Gaussian_Noise(test_data, percentage)
+df = pd.read_csv('test_column.csv', index_col=0)
+test_data = df['jet_1_pt'].values
 
-def test_gaussian_average_value():
+def test_gaussian_average_value_0():
+    percentage = 0.0
+    noisy_data = Gaussian_Noise(test_data, percentage)
     average_percentagee = np.average(np.abs(np.divide((noisy_data - test_data), test_data) * 100))
-    print(average_percentagee)
+    assert (average_percentagee == 0)
+
+def test_gaussian_average_value_10():
+    percentage = 0.1
+    noisy_data = Gaussian_Noise(test_data, percentage)
+    average_percentagee = np.average(np.abs(np.divide((noisy_data - test_data), test_data) * 100))
+    assert (5 <= average_percentagee  <= 15)
+
+def test_gaussian_average_value_20():
+    percentage = 0.2
+    noisy_data = Gaussian_Noise(test_data, percentage)
+    average_percentagee = np.average(np.abs(np.divide((noisy_data - test_data), test_data) * 100))
     assert (15 <= average_percentagee  <= 25)
+
+def test_gaussian_average_value_25():
+    percentage = 0.25
+    noisy_data = Gaussian_Noise(test_data, percentage)
+    average_percentagee = np.average(np.abs(np.divide((noisy_data - test_data), test_data) * 100))
+    assert (15  <= average_percentagee  <= 30)
+
+def test_gaussian_average_value_50():
+    percentage = 0.5
+    noisy_data = Gaussian_Noise(test_data, percentage)
+    average_percentagee = np.average(np.abs(np.divide((noisy_data - test_data), test_data) * 100))
+    assert ((percentage - (percentage * 0.20)) * 100  <= (percentage + (percentage * 0.20)) * 100 )
+
+def test_gaussian_random_state_is_set():
+    percentage = 0.2
+    noisy_data_1 = Gaussian_Noise(test_data, percentage, random_state=10)
+    noisy_data_2 = Gaussian_Noise(test_data, percentage, random_state=10)
+    assert (noisy_data_1[0] == noisy_data_2[0])
+
+def test_gaussian_random_state_not_set():
+    percentage = 0.1
+    noisy_data_1 = Gaussian_Noise(test_data, percentage, random_state=10)
+    noisy_data_2 = Gaussian_Noise(test_data, percentage, random_state=11)
+    assert (noisy_data_1[0] != noisy_data_2[0])
