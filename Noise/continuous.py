7fae5ae4eee207f79423f549c0f87b4510d4383f
Isabel <isafoster@live.com>
2023-02-15 12:32:54 +0100

    removed testng

--- Noise/continuous.py
+++ Noise/continuous.py
@@ -14,15 +14,4 @@ def Gaussian_Noise(clean_data, percentage, feature_name=None, random_state=None)
     if (isinstance(clean_data, (np.ndarray, np.generic, list))):
         noise = (clean_data * percentage) * np.random.normal(0, 1, len(clean_data))
         return clean_data + noise
-    print("Nope issues!!")
-
-
-if (__name__=="__main__"):
-    df = pd.read_csv('../tests/test_column.csv', index_col=0)
-    test_data = df['jet_1_pt'].values
-    print(type(df))
-    print(type(test_data))
-    df_2 = Gaussian_Noise(df, percentage=0.1, feature_name='jet_1_pt', random_state=2)
-    array_1 = Gaussian_Noise(test_data, percentage=0.1, random_state=2)
-    print(df_2)
-    print(array_1)
+    
