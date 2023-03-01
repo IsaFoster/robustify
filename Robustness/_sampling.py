5eac593510a00d62ebb4beed84b4a767094a4920
Isabel <isafoster@live.com>
2023-03-01 14:53:18 +0100

    Issue #20 noise corruption returns corrupted data

--- Robustness/_sampling.py
+++ Robustness/_sampling.py
@@ -1,4 +1,8 @@
 def sampleData(df, labelColumn, frac, random_state=None):
+    if frac == 1:
+        y = df[[labelColumn]]
+        X = df.drop([labelColumn], axis=1)
+        return X, y
     df_sampled = df.sample(frac=frac, replace=True, random_state=random_state)
     y_sampled = df_sampled[[labelColumn]]
     X_sampled = df_sampled.drop([labelColumn], axis=1)
--- Robustness/noiseCorruptions.py
+++ Robustness/noiseCorruptions.py
@@ -82,29 +82,43 @@ def baseline(df_train, X_test, y_test, model, labelColumn=None, random_state=Non
         baseline_results.loc[len(baseline_results.index)] = [feature_name, value, variance, accuracy]
     return baseline_results
 
+def fill_in_missing_columns(corrupted_df, df_train):
+    for column_name in corrupted_df:
+        if corrupted_df[column_name].isnull().all(): 
+            corrupted_df[column_name] = df_train[column_name].values
+    return corrupted_df
+
 def all(df_train, X_test, y_test, model, corruption_dict, corruptions, labelColumn=None, random_state=None, plot=True):
-    if (random_state):
-        set_random_seed(random_state)
+    set_random_seed(random_state)
+    corrupted_df = pd.DataFrame(columns=list(df_train))
     baseline_results = baseline(df_train, X_test, y_test, model, labelColumn)
     randomlist = random.sample(range(1, 1000), corruptions)
     progress_bar = initialize_progress_bar(corruption_dict, corruptions)
     for method in list(corruption_dict.items()):
         method_name = method[0]
-        corruption_result, measured_property = corruptData(df_train, X_test, y_test, model, method, randomlist, labelColumn, random_state, progress_bar)
+        method_corrupt_df, corruption_result, measured_property = corruptData(df_train, X_test, y_test, model, method, randomlist, labelColumn, random_state, progress_bar)
+        for column_name in list(method_corrupt_df):
+            corrupted_df[column_name] = method_corrupt_df[column_name].values
         if (plot):
             plotData(baseline_results, corruption_result, str(model), corruptions, measured_property, method_name)
+    corrupted_df = fill_in_missing_columns(corrupted_df, df_train)
     progress_bar.close()
+    return corrupted_df, corruption_result
 
 def corruptData(df_train, X_test, y_test, model, method, randomlist, labelColumn, random_state, progress_bar):
     corruption_result = pd.DataFrame(columns=['feature_name', 'level', 'value', 'variance', 'accuracy'])
     feature_names, levels = getLevels(method)
+    method_corrupt_df = pd.DataFrame(columns=feature_names)
     for level in levels: 
         for feature_name in feature_names:
             average_value = []
             average_accuracy = []
             average_variance = []
             for random in randomlist:
-                X, y = sampleData(df_train, labelColumn, 0.4, random_state=random)
+                if (random == randomlist[-1]): 
+                    X, y = sampleData(df_train, labelColumn, 1, random_state=random)
+                else: 
+                    X, y = sampleData(df_train, labelColumn, 0.4, random_state=random)
                 X = filter_on_method(X, method[0], feature_name, level, random_state)  # TODO: no point in passing the whole DF to change one column
                 average_variance.append(np.var(X[feature_name]))
                 model = train_model(model, X, y)
@@ -113,11 +127,12 @@ def corruptData(df_train, X_test, y_test, model, method, randomlist, labelColumn
                 average_value.append(measured_value)
                 average_accuracy.append(accuracy_score(y_test, model.predict(X_test)))
                 progress_bar.update(1)
+            method_corrupt_df[feature_name] = X[feature_name].values
             average_variance = np.average(average_variance)
             average_value = np.average(average_value)
             average_accuracy = np.average(average_accuracy)
             corruption_result.loc[len(corruption_result.index)] = [feature_name, level, average_value, average_variance, average_accuracy]
-    return corruption_result, measured_property
+    return method_corrupt_df, corruption_result, measured_property
 
 def plotData(baseline_results, corruption_result, model_name, corruptions, measured_property, method_name):
     features = corruption_result['feature_name'].values
