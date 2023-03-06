9040b013199c04740e785c7d93414c2dbdb56ae6
Isabel <isafoster@live.com>
2023-03-06 23:57:45 +0100

    Issue #20 added binomial noise

--- Noise/discrete.py
+++ Noise/discrete.py
@@ -1,16 +1,26 @@
 import numpy as np
 import pandas as pd
-import plotly.graph_objects as go
 
-def Binary_noise():
-    pass
+def Binomial_noise(clean_data, level, feature_name=None, random_state=None):
+    np.random.seed(random_state)
+    if (isinstance(clean_data, pd.DataFrame)):
+        data_col = clean_data[feature_name]
+        mask = np.random.binomial(1, level, data_col.shape[0]).astype(bool)
+        clean_data[feature_name] = 1 - mask
+        return clean_data
+    if (isinstance(clean_data, (np.ndarray, np.generic))):
+        mask = np.random.binomial(1, level, data_col.shape[0]).astype(bool)
+        return 1 - mask 
 
 def Poisson_noise(clean_data, feature_name=None, random_state=None):
     np.random.seed(random_state)
     if (isinstance(clean_data, pd.DataFrame)):
         data_col = clean_data[feature_name]
-        noise = np.random.poisson(np.mean(data_col), len(data_col))
+        print(np.unique(clean_data[feature_name], return_counts=True))
+        noise = np.random.poisson(lam=data_col, size=data_col.shape[0])
+        print(np.unique(noise, return_counts=True))
         clean_data[feature_name] = data_col + noise
+        print(np.unique(clean_data[feature_name], return_counts=True))
         return clean_data
     if (isinstance(clean_data, (np.ndarray, np.generic))):
         noise = np.random.poisson(np.mean(clean_data), len(clean_data))
--- Robustness/noiseCorruptions.py
+++ Robustness/noiseCorruptions.py
@@ -6,21 +6,17 @@ import pandas as pd
 from tqdm import tqdm
 import random
 from Noise.continuous import Gaussian_Noise
-from Noise.discrete import Poisson_noise
+from Noise.discrete import Poisson_noise, Binomial_noise
 
 def other(df, feature_name):
     return df[feature_name] * (-1) #TODO: makes no sense to plot this 
-
-def another(df, feature_name, level):
-    # TODO: should be based of probability and value of other feature
-    return df[feature_name] + level
 
 '********************************************************************************************************************************'
 
 def filter_on_method(df, method, feature_name, level=None, random_state=None):
     switcher = {
         'other': lambda: other(df, feature_name, level),
-        'another': lambda: another(df, feature_name),
+        'Binomial': lambda: Binomial_noise(df, level, feature_name, random_state),
         'Gaussian': lambda: Gaussian_Noise(df, level, feature_name, random_state),
         'Poisson': lambda: Poisson_noise(df, feature_name, random_state)
     }
@@ -46,18 +42,18 @@ def get_results(model, index):
 
 
 def getLevels(methodSpecification):
-    method = methodSpecification[0]
-    if (method == "Gaussian"):
-        return methodSpecification[1][0], methodSpecification[1][1]
+    method = list(methodSpecification.keys())[0]
+    if (method == "Gaussian" or method == "Binomial"):
+        return list(methodSpecification.values())[0][0], list(methodSpecification.values())[0][1]
     elif (method == "Poisson"):
-        return methodSpecification[1], [-1]
+        return list(methodSpecification.values())[0], [-1]
     else:
         print('Error getting values')
         print(type(methodSpecification))
 
-def initialize_progress_bar(corruption_dict, corruptions):
+def initialize_progress_bar(corruption_list, corruptions):
     total = 0 
-    for item in list(corruption_dict.items()):
+    for item in list(corruption_list):
         feature_names, levels = getLevels(item)
         total += ((len(feature_names) * len(levels)) * corruptions)
     return tqdm(total=total, desc="Total progress: ", position=0)
@@ -87,22 +83,22 @@ def fill_in_missing_columns(corrupted_df, df_train):
             corrupted_df[column_name] = df_train[column_name].values
     return corrupted_df
 
-def all(df_train, X_test, y_test, model, corruption_dict, corruptions, labelColumn=None, random_state=None, plot=True):
+def all(df_train, X_test, y_test, model, corruption_list, corruptions, labelColumn=None, random_state=None, plot=True):
     set_random_seed(random_state)
     corrupted_df = pd.DataFrame(columns=list(df_train))
     baseline_results = baseline(df_train, X_test, y_test, model, labelColumn)
     randomlist = random.sample(range(1, 1000), corruptions)
-    progress_bar = initialize_progress_bar(corruption_dict, corruptions)
-    for method in list(corruption_dict.items()):
-        method_name = method[0]
+    progress_bar = initialize_progress_bar(corruption_list, corruptions)
+    for method in list(corruption_list):
+        method_name = list(method.keys())[0]
         method_corrupt_df, corruption_result, measured_property = corruptData(df_train, X_test, y_test, model, method, randomlist, labelColumn, random_state, progress_bar)
         for column_name in list(method_corrupt_df):
             corrupted_df[column_name] = method_corrupt_df[column_name].values  
-    if (plot):
-        fig_1, fig_2, fig_3 = plotData(baseline_results, corruption_result, str(model), corruptions, measured_property, method_name)
-        fig_1.show()
-        fig_2.show()
-        fig_3.show()
+        if (plot):
+            fig_1, fig_2, fig_3 = plotData(baseline_results, corruption_result, str(model), corruptions, measured_property, method_name)
+            fig_1.show()
+            #fig_2.show()
+            #fig_3.show()
     corrupted_df = fill_in_missing_columns(corrupted_df, df_train)
     progress_bar.close()
     return corrupted_df, corruption_result
@@ -121,7 +117,7 @@ def corruptData(df_train, X_test, y_test, model, method, randomlist, labelColumn
                     X, y = sampleData(df_train, labelColumn, 1, random_state=random)
                 else: 
                     X, y = sampleData(df_train, labelColumn, 0.4, random_state=random)
-                X = filter_on_method(X, method[0], feature_name, level, random_state)  # TODO: no point in passing the whole DF to change one column
+                X = filter_on_method(X, list(method.keys())[0], feature_name, level, random_state)  # TODO: no point in passing the whole DF to change one column
                 average_variance.append(np.var(X[feature_name]))
                 model = train_model(model, X, y)
                 index = df_train.columns.get_loc(feature_name)
