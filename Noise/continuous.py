f2408c23faac29c7dd9c93603b89e2884f447e73
Isabel <isafoster@live.com>
2023-03-06 23:58:00 +0100

    code cleanup

--- Noise/continuous.py
+++ Noise/continuous.py
@@ -1,9 +1,6 @@
 import numpy as np
 import pandas as pd
 
-def _type_check(clean_data, ):
-    return 
-
 def Gaussian_Noise(clean_data, percentage, feature_name=None, random_state=None):
     np.random.seed(random_state)
     if (isinstance(clean_data, pd.DataFrame)):
