a3eca3b9fbab65265b562a465e9838f42313b31b
Isabel <isafoster@live.com>
2023-05-04 15:29:05 +0200

    Issue #32 groundwork for custom scorers

--- Robustness/_scorers.py
+++ Robustness/_scorers.py
@@ -1,8 +1,19 @@
 from sklearn import metrics
 
+def get_scorer(metric):
+    if isinstance(metric, str):
+        return get_scorer_sckit_learn(metric)
+    elif hasattr(metric, '__call__'):
+        return get_custom_scorer(metric)
 def get_scorer_sckit_learn(metric):
     # from https://scikit-learn.org/stable/modules/model_evaluation.html
     try: 
         return metrics.get_scorer(metric)
     except:
         print("not recognized")
+
+def get_custom_scorer(metric):
+    try:
+        print("custom scorer!")
+        return metric
+    except: raise Exception ("soemthing went wrong")
