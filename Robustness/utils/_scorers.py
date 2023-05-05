76c5c8df905ff22a2cbffc9f2e1fd2680b9e14d0
Isabel <isafoster@live.com>
2023-05-05 17:05:37 +0200

    Issue #32 added exceptions to custom score methods

--- Robustness/utils/_scorers.py
+++ Robustness/utils/_scorers.py
@@ -1,6 +1,7 @@
 from sklearn import metrics
 from inspect import signature
 from Robustness.utils._transform import convert_to_numpy
+from Robustness.utils._predict import get_prediction
 
 def get_scorer(metric, model, X_test, y_test, custom_predict):
     if isinstance(metric, str):
@@ -8,28 +9,33 @@ def get_scorer(metric, model, X_test, y_test, custom_predict):
     elif hasattr(metric, '__call__'):
         return get_custom_scorer(metric, model, X_test, y_test, custom_predict)
     else:
-        raise Exception("No usabel scorer object found")
+        raise ValueError("No scorer object found. Type {} is not callable".format(type(metric)))
     
 def get_scorer_sckit_learn(metric, model, X_test, y_test, custom_predict):
     try: 
         scorer = metrics.get_scorer(metric)
-        if custom_predict:
-            return scorer._score_func(y_test, custom_predict(model, X_test))
-        else: 
-            return scorer._score_func(y_test, model.predict(X_test))
-    except:
-        print("Could not find scorer '{}', see https://scikit-learn.org/stable/modules/model_evaluation.html for compatible scorers, or make a custom scoring function".format(metric))
-
+        scorer._score_func(y_test, get_prediction(model, X_test, custom_predict)) 
+    except Exception:
+        raise
 def get_custom_scorer(metric, model, X_test, y_test, custom_predict):
     try:
         X_test = convert_to_numpy(X_test)
         y_test = convert_to_numpy(y_test)
-        y_pred = custom_predict(model, X_test)
-        y_pred = convert_to_numpy(y_pred)
-
+        y_pred = get_prediction(model, X_test, custom_predict)
         sig = signature(metric)
         if len(sig.parameters) ==3:
-            return metric(model, y_pred, y_test)
+            score = metric(model, y_pred, y_test)
         else:
-            return metric(y_pred, y_test)
-    except: raise Exception ("soemthing went wrong")  
+            score = metric(y_pred, y_test)
+        validate_score(score)
+        return score 
+    except Exception:
+        raise
+
+def validate_score(score):
+    if score == None or isinstance(score, (str, bool)):
+        raise TypeError("Score must be a quantitative value, not {}".format(type(score)))  
+    if len(score) != 1 or isinstance(score, (list, dict, tuple)):
+        raise ValueError("Score must be a single value, not {}".format(type(score)))
+
+    
