17ebc09a5e61fffb0f48b449af3695eef78c577c
Isabel <isafoster@live.com>
2023-05-25 14:58:23 +0200

    Issue #79 improved imports

---
+++ robustify/utils/__init__.py
@@ -0,0 +1,9 @@
+from ._filter import filter_on_method, get_feature_name_from_index, get_levels
+from ._importances import filter_on_importance_method, check_for_deafult_properties, calculate_permuation_importances, calculate_eli5_importances, calculate_lime_importances, calculate_shap_importances
+from ._plot import plot_data
+from ._predict import get_prediction, verify_predictions
+from ._progress import initialize_progress_bar
+from ._sampling import sample_data
+from ._scorers import get_scorer, get_scorer_sckit_learn, get_custom_scorer, validate_score
+from ._train import custom_train_model, train_model ,reset_model, train_baseline
+from ._transform import convert_to_numpy, df_from_array, check_corruptions, fill_missing_columns
