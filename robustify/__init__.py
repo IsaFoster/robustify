80d7c8f54f1de04ccc571cab0455a134c3c45f3e
Isabel <isafoster@live.com>
2023-05-25 20:42:03 +0200

    Issue #79 snake_case rename

--- robustify/__init__.py
+++ robustify/__init__.py
@@ -1,6 +1,6 @@
 # Simplified imports
 from robustify.noise_corruptions import corrupt_data
-from robustify.noise.continuous import gaussian_Noise
+from robustify.noise.continuous import gaussian_noise
 from robustify.noise.discrete import poisson_noise, binomial_noise
 
 from . import utils
