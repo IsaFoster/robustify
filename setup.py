1784524e639a38e33c0e4a9ba993197ad5614c16
Isabel <60882878+IsaFoster@users.noreply.github.com>
2023-05-27 23:08:05 +0200

    Updated release version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import find_packages, setup
 setup(
     name='RobustifyToolkit',
     packages=find_packages(include=['robustify', 'robustify.noise', 'robustify.utils', 'tests']),
-    version='0.1.0',
+    version='0.1.1',
     readme="README.md",
     description="Robustify:" ,
     long_description=long_description,
