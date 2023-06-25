73ea4b5a23a8a5a169a3ede2c08b8b1b294bbd0f
Isabel <isafoster@live.com>
2023-06-25 19:26:50 +0200

    Issue #100 added exceptions to importances

--- robustify/utils/_importances.py
+++ robustify/utils/_importances.py
@@ -47,91 +47,102 @@ def check_for_deafult_properties(model, index, X, y, X_test, y_test, random_stat
         return calculate_permuation_importances(model, index, X, y, X_test, y_test, random_state, scoring)
 
 def calculate_permuation_importances(model, index, X, y, X_test, y_test, random_state, scoring):
-    measured_property = 'permutation importance'
-    importances = permutation_importance(model, convert_to_numpy(X_test), convert_to_numpy(y_test),
-                                            n_repeats=1, random_state=random_state,
-                                            n_jobs=-1, scoring=scoring)
-    if index is not None:
-        return importances.importances_mean[index], measured_property
-    else:
-        return importances.importances_mean, measured_property
+    try:
+        measured_property = 'permutation importance'
+        importances = permutation_importance(model, convert_to_numpy(X_test), convert_to_numpy(y_test),
+                                                n_repeats=1, random_state=random_state,
+                                                n_jobs=-1, scoring=scoring)
+        if index is not None:
+            return importances.importances_mean[index], measured_property
+        else:
+            return importances.importances_mean, measured_property
+    except Exception as e:
+        raise Exception("Could not calculate permutation importance:", e)
 
 def calculate_eli5_importances(model, index, X, y, X_test, y_test, random_state, scoring):
-    importances = PermutationImportance(model, scoring=scoring, random_state=random_state,
-                                        n_iter=1, cv="prefit", refit=False).fit(X_test, y_test)
-    measured_property = "eli5 permutation importance"
-    if index is not None:
-        return importances.feature_importances_[index], measured_property
-    else:
-        return importances.feature_importances_, measured_property
+    try:
+        importances = PermutationImportance(model, scoring=scoring, random_state=random_state,
+                                            n_iter=1, cv="prefit", refit=False).fit(X_test, y_test)
+        measured_property = "eli5 permutation importance"
+        if index is not None:
+            return importances.feature_importances_[index], measured_property
+        else:
+            return importances.feature_importances_, measured_property
+    except Exception as e:
+        raise Exception("Could not calculate permutation importance:", e)
 
 def calculate_lime_importances(model, index, X, X_test, custom_predict):
-    measured_property = "lime explainer"
-    explainer_classification = lime_tabular.LimeTabularExplainer(
-        training_data=X.to_numpy(),
-        feature_names=X.columns.tolist(),
-        class_names=['data_type'],
-        mode='classification',
-        verbose=False)
-    explainer_regression = lime_tabular.LimeTabularExplainer(
-        training_data=X.to_numpy(),
-        feature_names=X.columns.tolist(),
-        class_names=['data_type'],
-        mode='regression',
-        verbose=False)
-    values = []
-    values_df = pd.DataFrame(columns=list(range(len(X.columns))))
-    for i in range(int((X.shape[0]/10))):
-        if custom_predict:
-            predict_fn_rf = lambda x: custom_predict(model, x).astype(float)
-        elif is_keras_model(model):
-            predict_fn_rf = lambda x: model.predict(x, verbose=0).astype(float)
-        else:
-            predict_fn_rf = lambda x: model.predict_proba(x).astype(float)
-        try:
-            exp = explainer_classification.explain_instance(X_test.iloc[i], predict_fn_rf, num_features=len(X.columns.tolist()))
-        except: 
-            exp = explainer_regression.explain_instance(X_test.iloc[i], predict_fn_rf, num_features=len(X.columns.tolist()))
-        dic = dict(list(exp.as_map().values())[0])
+    try:
+        measured_property = "lime explainer"
+        explainer_classification = lime_tabular.LimeTabularExplainer(
+            training_data=X.to_numpy(),
+            feature_names=X.columns.tolist(),
+            class_names=['data_type'],
+            mode='classification',
+            verbose=False)
+        explainer_regression = lime_tabular.LimeTabularExplainer(
+            training_data=X.to_numpy(),
+            feature_names=X.columns.tolist(),
+            class_names=['data_type'],
+            mode='regression',
+            verbose=False)
+        values = []
+        values_df = pd.DataFrame(columns=list(range(len(X.columns))))
+        for i in range(int((X.shape[0]/10))):
+            if custom_predict:
+                predict_fn_rf = lambda x: custom_predict(model, x).astype(float)
+            elif is_keras_model(model):
+                predict_fn_rf = lambda x: model.predict(x, verbose=0).astype(float)
+            else:
+                predict_fn_rf = lambda x: model.predict_proba(x).astype(float)
+            try:
+                exp = explainer_classification.explain_instance(X_test.iloc[i], predict_fn_rf, num_features=len(X.columns.tolist()))
+            except: 
+                exp = explainer_regression.explain_instance(X_test.iloc[i], predict_fn_rf, num_features=len(X.columns.tolist()))
+            dic = dict(list(exp.as_map().values())[0])
+            if index is not None:
+                values.append(dic.get(index))
+            else:
+                limes = pd.DataFrame(columns=list(dic.keys()))
+                limes.loc[len(limes.index)] = list(dic.values())
+                values_df = pd.concat([values_df, limes])
         if index is not None:
-            values.append(dic.get(index))
+            average_value = np.mean(values, axis=0)
+            return average_value, measured_property
         else:
-            limes = pd.DataFrame(columns=list(dic.keys()))
-            limes.loc[len(limes.index)] = list(dic.values())
-            values_df = pd.concat([values_df, limes])
-    if index is not None:
-        average_value = np.mean(values, axis=0)
-        return average_value, measured_property
-    else:
-        average_value = values_df.mean(axis=0)
-        return average_value, measured_property
+            average_value = values_df.mean(axis=0)
+            return average_value, measured_property
+    except Exception as e:
+        raise Exception("Could not calculate lime importances:", e)
 
 def calculate_shap_importances(model, index, X, X_test, random_state, custom_predict):
-    measured_property = "shap values"
-    if custom_predict:
-        model_pred = lambda x: custom_predict(model, x)
-        explainer = shap.Explainer(model_pred, X, seed=random_state, silent=True)
-        shap_values = explainer(X)
-        average_values = [sum(sub_list) / len(sub_list) for sub_list in zip(*shap_values.values)]
-    elif is_keras_model(model):
-        average_values = shap_values_keras(model, X, X_test, random_state)
-    elif is_tree_model(model):
-        average_values =shap_values_tree(model, X, X_test, random_state)
-    else:
-        if hasattr(model, "predict_proba"):
-            explainer = shap.Explainer(model.predict, X, seed=random_state, silent=True)
-            shap_values = explainer(X_test)
+    try:
+        measured_property = "shap values"
+        if custom_predict:
+            model_pred = lambda x: custom_predict(model, x)
+            explainer = shap.Explainer(model_pred, X, seed=random_state, silent=True)
+            shap_values = explainer(X)
+            average_values = [sum(sub_list) / len(sub_list) for sub_list in zip(*shap_values.values)]
+        elif is_keras_model(model):
+            average_values = shap_values_keras(model, X, X_test, random_state)
+        elif is_tree_model(model):
+            average_values =shap_values_tree(model, X, X_test, random_state)
         else:
-            f = lambda x: model.predict(x)
-            med = X.median().values.reshape((1,X.shape[1]))
-            explainer = shap.Explainer(f, med, seed=random_state, silent=True)
-            shap_values = explainer(X_test) 
-        average_values = [sum(sub_list) / len(sub_list) for sub_list in zip(*shap_values.values)]
-    if index is not None:
-        return average_values[index], measured_property   
-    else:
-        return average_values, measured_property    
-    raise Exception("Could not compute shap importances")
+            if hasattr(model, "predict_proba"):
+                explainer = shap.Explainer(model.predict, X, seed=random_state, silent=True)
+                shap_values = explainer(X_test)
+            else:
+                f = lambda x: model.predict(x)
+                med = X.median().values.reshape((1,X.shape[1]))
+                explainer = shap.Explainer(f, med, seed=random_state, silent=True)
+                shap_values = explainer(X_test) 
+            average_values = [sum(sub_list) / len(sub_list) for sub_list in zip(*shap_values.values)]
+        if index is not None:
+            return average_values[index], measured_property   
+        else:
+            return average_values, measured_property    
+    except Exception as e:
+        raise Exception("Could not calculate shap values:", e)
     
 def shap_values_keras(model, X, X_test, random_state):
     X_train_summary = shap.kmeans(X, 50)
